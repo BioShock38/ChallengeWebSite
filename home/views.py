97f93194a44c17c5d15f033cba954e1c9c2d51dd
cayek <cayek@timc-bcm-15.imag.fr>
2015-07-02 18:08:18 +0200

    new design git status

---
+++ home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from login.forms import *
+from django.http import HttpResponseRedirect
+
+
+def home(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+            email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request, {
+    'form': form
+    })
+
+    return render_to_response('home/home.html', variables)
