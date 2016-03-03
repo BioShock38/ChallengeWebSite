1cb3f1cad8c046ccc542baa982388f819723eee6
challengewebstite <null>
2016-03-03 14:36:47 +0100

    deploy script

--- deploy.py
+++ deploy.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-import paramiko
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect("152.77.240.65", username="challengewebsite", password="timcimag" )
-stdin, stdout, stderr = client.exec_command('cd ChallengeWebSite; pwd; git pull')
-stdin, stdout, stderr = client.exec_command('pwd')
-print "stderr: ", stderr.readlines()
-print "pwd: ", stdout.readlines()
+import os
+
+# git pull
+os.system("git pull")
+
+
+# Migration
+os.system("./manage.py makemigrations")
+os.system("./manage.py migrate")
+os.system("./manage.py collectstatic")
