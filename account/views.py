313c5e2bb1ac1bda63649b6a618273963bbd7d16
Dias Alves <thomas.dias-alves@imag.fr>
2016-03-10 16:18:18 +0100

    history

--- account/views.py
+++ account/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 
+import json
+from datetime import datetime
+
 from challenge.models import Challenge, Dataset, Submission, Result
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, datetime):
+        serial = obj.isoformat()
+        return serial
+    raise TypeError ("Type not serializable")
 
 def index(request):
     if request.user.is_authenticated():
         l_results = Result.objects.filter(submission__simu__private=False) \
                                   .filter(submission__user=request.user.id)
-        context = {'results': l_results}
+        val_results = l_results.values('f1score',
+                                       'submission__simu',
+                                       'submission__simu__name',
+                                       'submission__date')
+        context = {'results': l_results,
+                   'json_results': json.dumps(list(val_results), default=json_serial)}
         return render(request, 'account/index.html', context)
