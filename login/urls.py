e2b41f4062d075a85b1bf2d3af8a8b1d2bf4ede7
cayek <cayek@timc-bcm-15.imag.fr>
2016-03-17 13:16:57 +0100

    warning removed

--- login/urls.py
+++ login/urls.py
@@ -6,7 +6,7 @@ import login.views as lv
 urlpatterns =  [
    url(r'^login/', django.contrib.auth.views.login, {'template_name': 'login/login.html'}, name = 'signin'),
     url(r'^home/$', lv.home),
-    url(r'^logout/$', 'django.contrib.auth.views.logout',{'next_page': '/'}),
+    url(r'^logout/$', django.contrib.auth.views.logout,{'next_page': '/'}),
     url(r'^register/$', lv.register, {'template_name': 'login/register.html'}, name = 'signup'),
     url(r'^register/success/$', lv.register_success),
     url(r'^confirm/(?P<activation_key>\w*)$', lv.confirm),
